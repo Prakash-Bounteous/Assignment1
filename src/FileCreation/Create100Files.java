8e9459fdeafd24d3c23408835444ef7a4959ad02
Kadamanchi Prakash Raj <kadamanchi.raj@bounteous.com>
2026-03-09 03:34:47 +0530

    created files

---
+++ src/FileCreation/Create100Files.java
@@ -0,0 +1,49 @@
+package FileCreation;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+class Create100Files {
+    Create100Files() throws IOException {
+
+        String folderPath = "C:\\Users\\KadamanchiPrakashRaj\\SB_Assignments\\Assignment1";
+        Files.createDirectories(Paths.get(folderPath));
+
+        for (int i = 1; i <= 100; i++) {
+
+            Path filePath = Paths.get(folderPath + "file" + i + ".txt");
+
+            int lines = i % 5;   // number of lines for this file
+
+            try (BufferedWriter writer = Files.newBufferedWriter(filePath)) {
+
+                for (int line = 1; line <= lines; line++) {
+
+                    StringBuilder sb = new StringBuilder();
+
+                    for (int word = 1; word <= line; word++) {
+
+                        String randomWord = UUID.randomUUID()
+                                .toString()
+                                .replace("-", " ")
+                                .split(" ")[0];   // take first word
+
+                        sb.append(randomWord);
+
+                        if (word < line) {
+                            sb.append(" ");
+                        }
+                    }
+
+                    writer.write(sb.toString());
+                    writer.newLine();
+                }
+            }
+        }
+
+        System.out.println("Files created successfully!");
+    }
+}
---
+++ src/FileCreation/Main.java
@@ -0,0 +1,9 @@
+package FileCreation;
+
+import java.io.IOException;
+
+public class Main {
+    static void main() throws IOException {
+        Create100Files chf= new Create100Files();
+    }
+}
